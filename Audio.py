e24b18be183f9d60bd42ec629f9344670121b623
Teo Mikelthun <teomiklethun@gmail.com>
2020-01-30 18:07:37 -0500

    Starter files.

---
+++ Audio.py
@@ -0,0 +1,10 @@
+class Audio:
+    
+    def __init__(self, file):
+        pass
+
+    def __init__(self, file_name):
+        pass
+
+    def __init__(self, other, start_time, stop_time):
+        pass
---
+++ Fingerprint.py
@@ -0,0 +1,9 @@
+class Fingerprint:
+    def __init__(self, spectrogram):
+        pass
+
+    def get_length(self):
+        pass
+
+    def __init__(self, other, start_time, stop_time):
+        pass
